2a6270929721e6aa637525680c3d80f861d63222
wwitekk <wagabunda@gmail.com>
2017-04-24 07:12:21 +0200

    added key attribute to list items

--- src/Components/days/dayList.js
+++ src/Components/days/dayList.js
@@ -3,10 +3,9 @@ import React from 'react';
 import DayItem from './dayItem';
 
 function DayList({ list }) {
-
   return (
     <ul>
-      {list.map(item => <DayItem item={item} />)}
+      {list.map(item => <DayItem key={item.id} item={item} />)}
     </ul>
   );
 }
